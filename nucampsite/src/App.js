d416e25c2c08753071681011836948a9f933ed37
“Jamie <jamiedevine77@gmail.com>
2023-01-07 13:24:59 -0600

    workshop

---
+++ nucampsite/src/App.js
@@ -0,0 +1,16 @@
+import Header from './components/Header';
+import './App.css';
+import Footer from './components/Footer';
+import CampsitesList from './features/campsites/CampsitesList';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+       <CampsitesList/>
+       <Footer/>
+    </div>
+  );
+}
+
+export default App;
