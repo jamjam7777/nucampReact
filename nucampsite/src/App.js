aaed6052a4248e01d7c46d1f4a25d23e4061d7d5
“Jamie <jamiedevine77@gmail.com>
2023-01-12 15:13:54 -0600

    campsitedetail

--- nucampsite/src/App.js
+++ nucampsite/src/App.js
@@ -3,6 +3,7 @@ import ContactPage from "./pages/ContactPage";
 import HomePage from "./pages/HomePage";
 import Header from "./components/Header";
 import "./App.css";
+import CampsiteDetailPage from "./pages/CapsiteDetailPage";
 import Footer from "./components/Footer";
 import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
 
@@ -14,6 +15,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="contact" element={<ContactPage />} />
         <Route path="directory" element={<CampsitesDirectoryPage />} />
+        <Route path='directory/:campsiteId'
+        element={<CampsiteDetailPage />} />
       </Routes>
       <Footer />
     </div>
