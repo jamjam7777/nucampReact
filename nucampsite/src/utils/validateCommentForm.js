8541a6227d2749a28025037fa6269c346ad9120a
“Jamie <jamiedevine77@gmail.com>
2023-01-27 13:47:51 -0600

    latest

--- nucampsite/src/utils/validateCommentForm.js
+++ nucampsite/src/utils/validateCommentForm.js
@@ -1,5 +1,9 @@
 export const validateContactForm = (values) => {
     const errors = {}; 
+
+    if (!values.rating) {
+        errors.rating = "Required";
+    }
     if (values.author.length < 2) {
         errors.author = 'Must be at least 2 characters.';
     
