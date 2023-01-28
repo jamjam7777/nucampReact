e5fead3f49d1a217dc7ff31f75fe19c5f9218fca
“Jamie <jamiedevine77@gmail.com>
2023-01-28 11:33:26 -0600

    userform

--- nucampsite/src/app/store.js
+++ nucampsite/src/app/store.js
@@ -4,13 +4,15 @@ import { partnersReducer } from '../features/partners/partnerSlice';
 import { promotionReducer } from '../features/promotions/promotionsSlice';
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { campsitesReducer } from '../features/campsites/campsitesSlice';
+import { userReducer } from '../features/user/userSlice';
 
 export const store = configureStore({
   reducer: {
     campsites: campsitesReducer,
     comments: commentsReducer,
     partners: partnersReducer,
-    promotions: promotionReducer
+    promotions: promotionReducer,
+    user: userReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 }); 
